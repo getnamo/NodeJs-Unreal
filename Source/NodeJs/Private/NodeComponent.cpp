7b3a8affe72089a1a25b3b714a32fd2b7cc15b4a
getnamo <getnamo@gmail.com>
2024-12-14 15:02:53 -0800

    scaffold early params

--- Source/NodeJs/Private/NodeComponent.cpp
+++ Source/NodeJs/Private/NodeComponent.cpp
@@ -11,18 +11,71 @@
 UNodeComponent::UNodeComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
+
+	SyncCLIParams();
 }
 
+void UNodeComponent::SyncCLIParams()
+{
+	if (!NodeJsProcessParams.bSyncCLIParams)
+	{
+		return;
+	}
+
+	//Main options
+	CLIParams.OptionalWorkingDirectory = FPaths::ConvertRelativePathToFull(FPaths::ProjectDir() + NodeJsProcessParams.ProcessPath);
+
+	FString ProcessNameForCurrentPlatform = NodeJsProcessParams.ProcessName;
+	
+#if PLATFORM_WINDOWS
+	ProcessNameForCurrentPlatform += TEXT(".exe");
+#endif
+
+	CLIParams.Url = CLIParams.OptionalWorkingDirectory + ProcessNameForCurrentPlatform;
+	CLIParams.bProcessInBytes = NodeJsProcessParams.bProcessInBytes;
+
+	//Basic params
+	CLIParams.bLaunchHidden = true;
+	CLIParams.bLaunchReallyHidden = true;
+	CLIParams.bOutputToGameThread = false;
+
+	//main process script to execute
+	CLIParams.Params = NodeJsProcessParams.ProcessScriptPath + NodeJsProcessParams.ProcessScriptName;
+}
+
+
+//UCLIProcessComponent overrides
+void UNodeComponent::StartProcess()
+{
+	//Ensure these are synced before we start
+	SyncCLIParams();
+
+	Super::StartProcess();
+}
+
+
+
+//UActorComponent overrides
+void UNodeComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+}
+
+void UNodeComponent::UninitializeComponent()
+{
+	Super::UninitializeComponent();
+}
 
 // Called when the game starts
 void UNodeComponent::BeginPlay()
 {
-
+	Super::BeginPlay();
 }
 
 
 void UNodeComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
+	Super::EndPlay(EndPlayReason);
 }
 
 
@@ -31,4 +84,3 @@ void UNodeComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorC
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
-
--- Source/NodeJs/Public/NodeComponent.h
+++ Source/NodeJs/Public/NodeComponent.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "CLIProcessComponent.h"
 #include "Components/ActorComponent.h"
 #include "SocketIOClientComponent.h"
 #include "NodeComponent.generated.h"
@@ -13,8 +14,53 @@ DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FNodeScriptPathSignature, FString, S
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FNodeScriptErrorSignature, FString, ScriptRelativePath, FString, ErrorMessage);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FNpmInstallResultSignature, bool, bIsInstalled, FString, ErrorMessage);
 
+USTRUCT(BlueprintType)
+struct FNodeJsProcessParams
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "NodeJs Params")
+	FString ProcessPath = TEXT("Plugins/NodeJs-Unreal/Source/ThirdParty/node/");
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "NodeJs Params")
+	FString ProcessName = TEXT("node");
+
+	//main entry point script that wraps the IPC bridge for communication handling
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "NodeJs Params")
+	FString ProcessScriptName = TEXT("process.js");
+
+	//This is relative to the process path
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "NodeJs Params")
+	FString ProcessScriptPath = TEXT("../../../Content/Scripts/");
+
+	//NB: not yet implemented
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "NodeJs Params")
+	bool bShareMainProcess = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "NodeJs Params")
+	bool bProcessInBytes = false;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "NodeJs Params")
+	bool bSyncCLIParams = true;
+};
+
+USTRUCT(BlueprintType)
+struct FNodeJsScriptParams
+{
+	GENERATED_USTRUCT_BODY()
+
+	//Relative to Project Root
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "NodeJs Params")
+	FString ScriptPathRoot = TEXT("Content/Scripts/");
+
+	//The script you want to run for this component
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "NodeJs Params")
+	FString Script = TEXT("script.js");
+};
+
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class NODEJS_API UNodeComponent : public UActorComponent
+class NODEJS_API UNodeComponent : public UCLIProcessComponent
 {
 	GENERATED_BODY()
 
@@ -40,14 +86,28 @@ public:
 	UPROPERTY(BlueprintAssignable, Category = "Npm Events")
 	FNpmInstallResultSignature OnNpmDependenciesResolved;
 
+	//Customize these for your script
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "NodeJs Parameters")
+	FNodeJsScriptParams ScriptParams;
+
+	//Core process parameters for establishing the process bridge
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "NodeJs Parameters")
+	FNodeJsProcessParams NodeJsProcessParams;
+
 	UNodeComponent();
+
+	void SyncCLIParams();
 
 protected:
 
+	//UCLIProcessComponent overrides
+	virtual void StartProcess() override;
+
+	//UActorComponent overrides
+	virtual void InitializeComponent() override;
+	virtual void UninitializeComponent() override;
 	virtual void BeginPlay() override;
-
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
-
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 private:
